1064c4491c3108fe25a3c90ff6927a57ecd2e4f6
ctrlaltdev <yorick@ctrlatldev.xyz>
2017-12-08 00:30:14 +0100

    adapted script to be launched every minute

--- lmgtfy.sh
+++ lmgtfy.sh
@@ -6,12 +6,19 @@ if [ ! -r hashes -o ! -w hashes ]
     exit 1;
 fi
 
-if [ "$(wc -l hashes | grep -o -P '.+(?= hashes)')" == "0" ]
-  then
-    echo No more hashes!
-    exit 1;
-fi
+for i in {0..59}
+do
 
-hash=$(head -1 hashes)
-curl -v --silent http://bfy.tw/${hash} --stderr - | grep -o -P '(?<=q=).*(?=")' >> queries
-tail -n +2 hashes > hashes.tmp && mv hashes.tmp hashes
+  if [ "$(wc -l hashes | grep -o -P '.+(?= hashes)')" == "0" ]
+    then
+      echo No more hashes!
+      exit 1;
+  fi
+
+  hash=$(head -1 hashes)
+  curl -v --silent http://bfy.tw/${hash} --stderr - | grep -o -P '(?<=q=).*(?=")' >> queries
+  tail -n +2 hashes > hashes.tmp && mv hashes.tmp hashes
+
+  sleep 0.5
+
+done
