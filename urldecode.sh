216f94981a90daa388c6aa469d5ab215a86e0bce
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-01-05 20:28:22 +0100

    fix urldecoding

--- urldecode.sh
+++ urldecode.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
-tar -czvf queries.raw.tar.gz queries
 lines=$(wc -l queries | grep -o -P '.+(?= queries)')
-for [ 0..${lines} ]
+i=1
+while [ "$i" -lt "$lines" ]
   do
     if [ "$(wc -l queries | grep -o -P '.+(?= queries)')" == "0" ]
       then
+        echo -ne "\n"
         echo Done
         exit 1;
     fi
-
-    query=$(head -1 queries)
-    querydecoded=$(python3 urldecode.py ${query})
-    echo ${querydecoded} >> queries.decoded
-    tail -n +2 queries > queries.tmp && mv queries.tmp queries
+    python3 urldecode.py "$(head -$i queries)" >> queries.decoded
+    echo -ne "${i} / ${lines}\r"
+    i=$[$i + 1]
 done
